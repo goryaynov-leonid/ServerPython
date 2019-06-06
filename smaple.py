30100a61dd14e6fc6c44b3f73a071df186ec3c6b
Leonid Goryaynov <lbg9992@mail.ru>
2019-06-06 16:55:52 +0700

    run server and handle requests

---
+++ smaple.py
@@ -0,0 +1,57 @@
+from cgi import parse_header, parse_multipart
+from urllib.parse import parse_qs
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        ctype, pdict = parse_header(self.headers['content-type'])
+        if ctype == 'multipart/form-data':
+            postvars = parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers['content-length'])
+            postvars = parse_qs(
+                self.rfile.read(length),
+                keep_blank_values=1)
+        else:
+            postvars = {}
+        return postvars
+        """
+                # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        post_data = post_data.decode('utf-8')
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        :return: 
+        """
+
+
+
+def run(server_class=HTTPServer, handler_class=S, port=9999):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    """
+        from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+    """
+    run()
