969ce7b84df3c6cf56fe67c9d570346f1b533df6
Leonid Goryaynov <lbg9992@mail.ru>
2019-06-08 20:37:51 +0700

    installed facenet

--- smaple.py
+++ smaple.py
@@ -1,18 +1,19 @@
-import cgi
+import torch
+import pandas as pd
+from torch.utils.data import DataLoader
 from http.server import BaseHTTPRequestHandler,HTTPServer
+import cgi
+
+import numpy as np
+import pymysql.cursors
+import facenet_pytorch as fp
+from torchvision import transforms, datasets
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-
-    def do_HEAD(self):
-        self._set_headers()
 
     def do_POST(self):
         self._set_headers()
@@ -33,6 +34,14 @@ class S(BaseHTTPRequestHandler):
         #TODO add another request type to create new user
 
 
+def getConnectionToDB():
+    connection = pymysql.connect(host='127.0.0.1',
+                                 user='root',
+                                 password='Root1234',
+                                 db='pictureusers',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
 def run(server_class=HTTPServer, handler_class=S, port=9999):
     server_address = ('', port)
@@ -40,6 +49,35 @@ def run(server_class=HTTPServer, handler_class=S, port=9999):
     print('Starting httpd...')
     httpd.serve_forever()
 
+def runFacenet(imagesPath = 'userImages'):
+    mtcnn = fp.MTCNN()
+    resnet = fp.InceptionResnetV1(pretrained='casia-webface').eval()
 
+    # Define a dataset and data loader
+    trans = transforms.Compose([
+        transforms.Resize(1024)
+    ])
+    dataset = datasets.ImageFolder(imagesPath, transform=trans)
+    dataset.idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
+    loader = DataLoader(dataset, collate_fn=lambda x: x[0])
+
+    # Perfom MTCNN facial detection
+    aligned = []
+    names = []
+    for x, y in loader:
+        x_aligned, prob = mtcnn(x, return_prob=True)
+        if x_aligned is not None:
+            print('Face detected with probability: {:8f}'.format(prob))
+            aligned.append(x_aligned)
+            names.append(dataset.idx_to_class[y])
+
+    # Calculate image embeddings
+    aligned = torch.stack(aligned)
+    embeddings = resnet(aligned)
+
+    # Print distance matrix for classes
+    dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
+    print(pd.DataFrame(dists, columns=names, index=names))
 if __name__ == "__main__":
+    runFacenet()
     run()
